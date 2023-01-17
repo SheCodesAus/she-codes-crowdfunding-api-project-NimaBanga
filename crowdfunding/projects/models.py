459d5e76065374f356fbec1a1f9d0841f2dd2edc
NimaBanga <114783983+NimaBanga@users.noreply.github.com>
2023-01-17 18:10:21 +1100

    first coding session on project session

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title=models.CharField(max_length=200) #used for shorter texts versus TextField
+    description=models.TextField()
+    goal=models.IntegerField()
+    image=models.URLField() #Helps to point to a source stored somewhere else
+    is_open=models.BooleanField()
+    date_created=models.DateTimeField(auto_now_add=True) #auto now add enable adding time of creation of project automatically
+    owner=models.CharField(max_length=200) #will need to change this from charfield to foreign key so it can reference id of the users once created
+    
+
+    
